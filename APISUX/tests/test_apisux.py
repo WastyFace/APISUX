efc399d56fa30ffdc0c1f137d03d849b2dfbd11d
WastyFace <wastyface93@gmail.com>
2022-06-15 23:23:36 -0500

    Update test

--- APISUX/tests/test_apisux.py
+++ APISUX/tests/test_apisux.py
@@ -112,3 +112,98 @@ def test_GetColonias():
     x = requests.get('http://127.0.0.1:9090/colonias')
     statusCode = x.status_code
     assert HTTPStatus.OK == statusCode
+
+def test_PostRuta():
+    global token
+    global bear
+    connection = httpClient.HTTPConnection('127.0.0.1:9090')
+    data = {
+        "nombre" : "Ruta Nueva",
+        "recorrido" : "Agua pasa por mi casa, cate de mi corazon",
+        "colonias" : "Colonia 1, Colonia 2, Colonia 3",
+        "imgPath" : "./img/RutaNueva.png"
+    }
+    bear = "Bearer " + token
+    data2 = json.dumps(data)
+    headers = {"Content-Type": "application/json", "Authorization": bear}
+    response = connection.request(
+        "POST",
+        "/rutas",
+        data2,
+        headers,
+    )
+    generalResponse = connection.getresponse()
+    statusCode = generalResponse.status
+    assert HTTPStatus.CREATED == statusCode
+
+def test_PatchRuta():
+    global token
+    global bear
+    connection = httpClient.HTTPConnection('127.0.0.1:9090')
+    data = {
+        "nombre" : "Ruta Nueva Editada",
+        "recorrido" : "Agua pasa por mi casa, cate de tu corazon",
+        "colonias" : "Colonia 1, Colonia 2, Colonia 3, Colonia 4",
+        "imgPath" : "./img/RutaNuevaEditada.png"
+    }
+    bear = "Bearer " + token
+    data2 = json.dumps(data)
+    headers = {"Content-Type": "application/json", "Authorization": bear}
+    response = connection.request(
+        "PATCH",
+        "/rutas/1",
+        data2,
+        headers,
+    )
+    generalResponse = connection.getresponse()
+    statusCode = generalResponse.status
+    assert HTTPStatus.OK == statusCode
+
+def test_DeleteRuta():
+    global token
+    global bear
+    connection = httpClient.HTTPConnection('127.0.0.1:9090')
+    data = {
+        "nombre" : "Ruta Nueva Editada",
+        "recorrido" : "Agua pasa por mi casa, cate de tu corazon",
+        "colonias" : "Colonia 1, Colonia 2, Colonia 3, Colonia 4",
+        "imgPath" : "./img/RutaNuevaEditada.png"
+    }
+    bear = "Bearer " + token
+    data2 = json.dumps(data)
+    headers = {"Content-Type": "application/json", "Authorization": bear}
+    response = connection.request(
+        "DELETE",
+        "/rutas/1",
+        data2,
+        headers,
+    )
+    generalResponse = connection.getresponse()
+    statusCode = generalResponse.status
+    assert HTTPStatus.NO_CONTENT == statusCode
+
+def test_GetRutas():
+    x = requests.get('http://127.0.0.1:9090/rutas')
+    statusCode = x.status_code
+    assert HTTPStatus.OK == statusCode
+
+def test_Logout():
+    global token
+    global bear
+    connection = httpClient.HTTPConnection('127.0.0.1:9090')
+    data = {
+        "username" : "admin1234",
+        "password" : "12345"
+    }
+    bear = "Bearer " + token
+    data2 = json.dumps(data)
+    headers = {"Content-Type": "application/json", "Authorization": bear}
+    response = connection.request(
+        "POST",
+        "/revoke",
+        data2,
+        headers,
+    )
+    generalResponse = connection.getresponse()
+    statusCode = generalResponse.status
+    assert HTTPStatus.OK == statusCode
