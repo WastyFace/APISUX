d8b5b400453f854be507241131c8bcd0e5849806
WastyFace <wastyface93@gmail.com>
2022-06-15 21:39:40 -0500

    Actualización pruebas

--- APISUX/tests/test_apisux.py
+++ APISUX/tests/test_apisux.py
@@ -2,6 +2,7 @@ from cgi import test
 from http import HTTPStatus
 import http.client as httpClient
 import json
+import requests
 
 def test_PostAdmin():
     connection = httpClient.HTTPConnection('127.0.0.1:9090')
@@ -15,6 +16,49 @@ def test_PostAdmin():
     response = connection.request(
         "POST",
         "/users",
+        data2,
+        headers,
+    )
+    generalResponse = connection.getresponse()
+    statusCode = generalResponse.status
+    assert HTTPStatus.CREATED == statusCode
+
+def test_Login():
+    global token
+    connection = httpClient.HTTPConnection('127.0.0.1:9090')
+    data = {
+        "username" : "admin1234",
+        "password" : "12345"
+    }
+    data2 = json.dumps(data)
+    headers = {"Content-Type": "application/json"}
+    response = connection.request(
+        "POST",
+        "/token",
+        data2,
+        headers,
+    )
+    generalResponse = connection.getresponse()
+    readResponse = generalResponse.read()
+    json_format = json.loads(readResponse)
+    statusCode = generalResponse.status
+    token = json_format["access_token"]
+    assert HTTPStatus.OK == statusCode
+
+def test_PostColonia():
+    global token
+    global bear
+    connection = httpClient.HTTPConnection('127.0.0.1:9090')
+    data = {
+        "nombre" : "Colonia Nueva",
+        "codigoPostal" : "290116"
+    }
+    bear = "Bearer " + token
+    data2 = json.dumps(data)
+    headers = {"Content-Type": "application/json", "Authorization": bear}
+    response = connection.request(
+        "POST",
+        "/colonias",
         data2,
         headers,
     )
