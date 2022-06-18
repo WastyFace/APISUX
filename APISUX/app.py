4e4bbe4f7b4d8b8524f5dc594f91487d23cf7f7b
WastyFace <wastyface93@gmail.com>
2022-06-17 19:21:31 -0500

    Update app.py

--- APISUX/app.py
+++ APISUX/app.py
@@ -1,7 +1,6 @@
-from flask import Flask, request
+from flask import Flask
 from flask_migrate import Migrate
 from flask_restful import Api
-from werkzeug.utils import secure_filename
 
 from config import Config
 from extensions import db, jwt
@@ -21,15 +20,17 @@ def create_app():
 
     return app
 
+
 def register_extensions(app):
     db.init_app(app)
-    migrate = Migrate(app, db)
+    Migrate(app, db)
     jwt.init_app(app)
 
     @jwt.token_in_blocklist_loader
     def check_if_token_in_blocklist(jwt_header, jwt_payload):
         jti = jwt_payload["jti"]
         return jti in block_list
+
 
 def register_resources(app):
     api = Api(app)
